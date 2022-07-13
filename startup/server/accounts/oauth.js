18103e9273f59611eafeabf13411eed62262a4f6
deliteser112 <74626440+deliteser112@users.noreply.github.com>
2022-07-13 15:00:10 +0900

    oauth was changed

--- startup/server/accounts/oauth.js
+++ startup/server/accounts/oauth.js
@@ -4,14 +4,12 @@ import { ServiceConfiguration } from 'meteor/service-configuration';
 if (Meteor.settings.private && Meteor.settings.private.OAuth) {
   const OAuthSettings = Meteor.settings.private.OAuth;
 
-  console.log('OAuthSettings', OAuthSettings);
-
   Object.keys(OAuthSettings).forEach((service) => {
     ServiceConfiguration.configurations.upsert({ service }, { $set: OAuthSettings[service] });
   });
 } else {
-  console.warn('OAuthSettings', Meteor.settings.private);
+  console.warn('OAuthSettings--->', Meteor.settings.private);
   console.warn(
-    'Woof! OAuth settings are not configured. OAuth login will not function. See the documentation for configuration instructions.',
+    'OAuth settings are configured, but the memory is out.',
   );
 }
